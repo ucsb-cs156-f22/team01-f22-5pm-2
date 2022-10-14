b1c189fc384ad84519c38b83229504932800a570
Bryan Huang <b_huang@cs.ucsb.edu>
2022-10-13 18:16:32 -0700

    bh - finished reddit query service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryService.java
@@ -6,12 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+import org.apache.tomcat.util.http.parser.EntityTag;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 @Slf4j
 @Service
@@ -30,9 +34,12 @@ public class RedditQueryService {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("User-Agent","spring-boot:cs156-team01:f22 (by /u/cgaucho)");
 
-        
-        return "";
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class);
+        return response.getBody();
     }
    
 
