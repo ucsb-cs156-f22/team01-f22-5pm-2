3dc38d365c3c7fa803fb2ecc653dffd9c103a56e
Bryan Huang <b_huang@cs.ucsb.edu>
2022-10-13 17:56:53 -0700

    bh - initialized reddit query service http headers

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryService.java
@@ -2,13 +2,18 @@ package edu.ucsb.cs156.spring.backenddemo.services;
 
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
-
+@Slf4j
 @Service
 public class RedditQueryService {
 
@@ -18,12 +23,17 @@ public class RedditQueryService {
         restTemplate = restTemplateBuilder.build();
     }
 
-    public static final String ENDPOINT = "";
+    public static final String ENDPOINT = "https://www.reddit.com/r/{subreddit}.json";
 
     public String getJSON(String subreddit) throws HttpClientErrorException {
+        log.info("subreddit={}", subreddit);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        
         return "";
     }
-
    
 
 }
