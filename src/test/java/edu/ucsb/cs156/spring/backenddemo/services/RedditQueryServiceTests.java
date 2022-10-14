aacf23d08cf6470081b7f8cf16e82b9d15a08bce
Bryan Huang <b_huang@cs.ucsb.edu>
2022-10-13 23:36:24 -0700

    bh - started reddit query service test file

---
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryServiceTests.java
@@ -0,0 +1,26 @@
+package edu.ucsb.cs156.spring.backenddemo.services;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.test.web.client.MockRestServiceServer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+@RestClientTest(RedditQueryService.class)
+public class RedditQueryServiceTests {
+    
+    @Autowired
+    private MockRestServiceServer mockRestServiceServer;
+
+    @Autowired
+    private RedditQueryService redditQueryService;
+
+    @Test
+    public void test_getJSON() throws URISyntaxException, UnsupportedEncodingException, JsonProcessingException {
+        String expectedURL = redditQueryService.ENDPOINT;
+    }
+}
